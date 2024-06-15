0bb2cc7e55982ac705a3cb266c58b12e33c9741f
RAJESHWARI CHOUDHURY <138126187+RajeshwariChoudhury@users.noreply.github.com>
2024-06-15 12:31:07 +0530

    Add files via upload

---
+++ hack2.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+ class hack2 {
+
+    public static void main(String[] args) {
+       
+        Scanner sc=new Scanner(System.in);
+        int i=sc.nextInt();
+        int j=sc.nextInt();
+        int k=sc.nextInt();
+        System.out.println(i);
+        System.out.println(j);
+        System.out.println(k);
+    }
+}

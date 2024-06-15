8e03860e0544844d40a529d536a9718ac54cac32
RAJESHWARI CHOUDHURY <138126187+RajeshwariChoudhury@users.noreply.github.com>
2024-06-15 12:22:22 +0530

    Add files via upload

---
+++ hack1.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.util.*;
+ class hack1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+            }
+}

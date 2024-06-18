69a34b10b79266dad59d85e75c22961b48a92c51
RAJESHWARI CHOUDHURY <138126187+RajeshwariChoudhury@users.noreply.github.com>
2024-06-18 09:55:06 +0530

    Add files via upload

---
+++ hack11.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class hack11 {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        in.close();
+
+        String s = Integer.toString(n);
+
+        if (n == Integer.parseInt(s)) {
+            System.out.println("Good job");
+        } else {
+            System.out.println("Wrong answer.");
+        }
+    }
+}

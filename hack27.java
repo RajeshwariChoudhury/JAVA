fa59b85694dae0be8b78f0c598714f77fbd2a7fb
RAJESHWARI CHOUDHURY <138126187+RajeshwariChoudhury@users.noreply.github.com>
2024-06-24 09:53:09 +0530

    Add files via upload

---
+++ hack27.java
@@ -0,0 +1,35 @@
+import java.util.*;
+class hack27{
+	
+	public static void main(String []argh)
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		while (sc.hasNext()) {
+			String input=sc.next();
+            System.out.println(checkIfBalanced(input,'{', '}')&&checkIfBalanced(input,'[', ']')&&checkIfBalanced(input,'(', ')'));
+            //Complete the code
+		}
+		
+	}
+    public static boolean checkIfBalanced(String input, char opening, char closing){
+        Stack<String> stack = new Stack<String>();
+            for(int i = 0; i < input.length();i++){
+            char c = input.charAt(i);
+            if(c == closing){
+                if(!stack.empty()){
+                stack.pop();
+                }else{
+                return false;
+                }
+            }
+            if(c == opening){
+                stack.push(String.valueOf(c));
+            }
+            } 
+        return stack.empty();
+        }
+}
+
+
+
